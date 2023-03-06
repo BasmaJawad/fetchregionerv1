d2d2822dc108afb221b3292d32a1fb8aaea84cb0
ERLMKea <erlm@kea.dk>
2023-03-06 11:34:55 +0100

    rest update photohref

--- js/createtable.js
+++ js/createtable.js
@@ -19,9 +19,52 @@ function createTable(kommune) {
     cell = row.insertCell(cellCount++)
     cell.innerHTML = kommune.region.navn
 
+    cell = row.insertCell(cellCount++)
+    let inpHrefPhoto = document.createElement("input")
+    inpHrefPhoto.type = "text"
+    inpHrefPhoto.setAttribute("value", kommune.hrefPhoto)
+    cell.appendChild(inpHrefPhoto)
 
+    cell = row.insertCell(cellCount++)
+    let pbUpdate = document.createElement("button")
+    pbUpdate.textContent = "Opdater"
+    pbUpdate.className = "buttonupdate"
+    pbUpdate.addEventListener('click', function () {
+        kommune.hrefPhoto = inpHrefPhoto.value;
+        updateKommune(kommune)
+    })
+    cell.appendChild(pbUpdate)
 
 }
+
+async function updateKommune(kommune) {
+    console.log(kommune.hrefPhoto)
+    console.log(kommune)
+    const response = await restUpdateKommune(kommune)
+    console.log(response)
+}
+
+async function restUpdateKommune(kommune) {
+    const url = "http://localhost:8080/kommune/" + kommune.kode;
+    const fetchOptions = {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: ""
+    }
+    const jsonString = JSON.stringify(kommune);
+    fetchOptions.body = jsonString;
+    //calls backend and wait for return
+    const response = await fetch(url, fetchOptions);
+    console.log(response);
+    if (!response.ok) {
+        out("Det gik ikke godt med update");
+    };
+    return response;
+}
+
+
 
 function actionCreateTable() {
     lstKommuner.forEach(createTable)
