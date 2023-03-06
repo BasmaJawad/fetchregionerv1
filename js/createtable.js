b31ada7c460014afa4ca68751d5626a7a31d3b3b
ERLMKea <erlm@kea.dk>
2023-03-06 10:46:58 +0100

    nu med style

--- js/createtable.js
+++ js/createtable.js
@@ -5,6 +5,22 @@ const tblKommune = document.getElementById("tblKommune")
 
 function createTable(kommune) {
     console.log(kommune.navn)
+    if (!kommune.kode) return;
+
+    let cellCount = 0
+    let rowCount = tblKommune.rows.length
+    let row = tblKommune.insertRow(rowCount)
+    let cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.kode
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.navn
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.region.navn
+
+
+
 }
 
 function actionCreateTable() {
