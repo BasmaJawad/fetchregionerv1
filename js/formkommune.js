317ef68ef6d7b3d9a9cf058743742a43c7e71bab
ERLMKea <erlm@kea.dk>
2023-03-07 14:50:36 +0100

    plainFormData nu med region hardkodet

--- js/formkommune.js
+++ js/formkommune.js
@@ -30,4 +30,29 @@ async function postFormData(url, formData) {
     const plainFormData = Object.fromEntries(formData.entries())
     console.log(plainFormData)
 
+    const region = {}
+    region.kode = "1085"
+    plainFormData.region = region
+
+    const formDataJsonString = JSON.stringify(plainFormData)
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: formDataJsonString
+    }
+
+    const response = await fetch(url, fetchOptions)
+
+    if (!response.ok) {
+        const errorMessage = await response.text()
+        throw new Error(errorMessage)
+    }
+    return response.json();
+
 }
+
+
+
