322e3154b49ffe8cf2a3907f5339a508c0017423
ERLMKea <erlm@kea.dk>
2023-03-07 14:04:22 +0100

    plainFormData

---
+++ js/formkommune.js
@@ -0,0 +1,33 @@
+console.log("Vi er i formkommune")
+
+document.addEventListener('DOMContentLoaded', createFormEventListener);
+let formKommune;
+
+function createFormEventListener(){
+    formKommune = document.getElementById("formKommune");
+    formKommune.addEventListener("submit", handleFormSubmit);
+}
+
+async function handleFormSubmit(event) {
+    //Vi handler submit her, i stedet for default html behaviour
+    event.preventDefault();
+    const form = event.currentTarget;
+    const url = form.action;
+    console.log(form)
+    console.log(url)
+    console.log(form === formKommune)
+    try {
+        const formData = new FormData(form)
+        console.log(formData)
+        const responseData = await postFormData(url, formData)
+    } catch (error) {
+        alert(error.message)
+        console.log(error)
+    }
+}
+
+async function postFormData(url, formData) {
+    const plainFormData = Object.fromEntries(formData.entries())
+    console.log(plainFormData)
+
+}
